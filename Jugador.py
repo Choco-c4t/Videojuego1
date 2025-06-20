6a04ca154f7905f12f571090dab25030eb63c887
Choco-c4t <138539780+Choco-c4t@users.noreply.github.com>
2025-06-19 23:14:53 -0500

    Jugador.py

--- Jugador.py
+++ Jugador.py
@@ -7,33 +7,18 @@ class Jugador:
         self.x = x
         self.y = y
         self.velocidad = 5
-        self.radio = 20
         self.balas = []
         self.vida = 100
         self.vida_maxima = 100
         self.sprite = SPRITE_JUGADOR
-        self.direccion = "derecha"  # Puede ser: "arriba", "abajo", "izquierda", "derecha"
-        self.invencible=False
-
-    def ser_invencible(self,miliseg=10000):
-        self.invencible=True
-
-    def rev_invenci(self):
-        if self.invencible:
-            self.invincible_time -= pygame.time.get_ticks()
-            if self.invincible_time <= 0:
-                self.invincible = False
-        # Actualiza la posición del jugador
-        self.rect.x += 1
-        if self.rect.right > screen_width:
-            self.rect.right = screen_width
-        # Dibuja el jugador
-        screen.blit(self.image, self.rect)
-
-    def make_invincible(self):
-        self.invincible = True
-        self.invincible_time = self.invincible_duration
-
+        self.direccion = "derecha"
+        self.width = 40 
+        self.height = 40
+        self.rect = pygame.Rect(self.x - self.width // 2, self.y - self.height // 2, self.width, self.height)
+        self.robo_vida_activo = False
+        self.tiempo_robo_vida = 0  # Rango
+        self.duracion_robo = 300
+        self.radio_robo_vida = 20
 
     def mover(self, teclas):
         if teclas[pygame.K_w]:
@@ -49,14 +34,20 @@ class Jugador:
             self.x += self.velocidad
             self.direccion = "derecha"
 
-        if self.x < self.radio:
-            self.x = self.radio
-        if self.x > ANCHO - self.radio:
-            self.x = ANCHO - self.radio
-        if self.y < self.radio:
-            self.y = self.radio
-        if self.y > ALTO - self.radio:
-            self.y = ALTO - self.radio
+        self.rect.center = (self.x, self.y)
+        
+        
+        if self.x < self.width // 2:
+            self.x = self.width // 2
+        if self.x > ANCHO - self.width // 2:
+            self.x = ANCHO - self.width // 2
+        if self.y < self.height // 2:
+            self.y = self.height // 2
+        if self.y > ALTO - self.height // 2:
+            self.y = ALTO - self.height // 2
+
+    def verificar_colision_enemigo(self, enemigo):
+        return self.rect.colliderect(enemigo.rect)
 
     def disparar(self):
         if len(self.balas) < 3:  # Max 3 balas
@@ -68,24 +59,22 @@ class Jugador:
                 self.balas.append(Bala(self.x, self.y, -1, 0))
             else:  # derecha
                 self.balas.append(Bala(self.x, self.y, 1, 0))
+        
+        if self.robo_vida_activo:
+            self.vida = min(self.vida + 5, self.vida_maxima) 
 
     def actualizar_balas(self, enemigos):
         balas_activas = []
-        
         for bala in self.balas:
             bala.actualizar()
-            
             golpeo = False
             for enemigo in enemigos:
-                distancia = ((bala.x - enemigo.x)**2 + (bala.y - enemigo.y)**2)**0.5
-                if distancia < enemigo.radio:
+                if bala.rect.colliderect(enemigo.rect):
                     enemigo.recibir_daño(10)
                     golpeo = True
-                    break
-            
+                    break           
             if not bala.fuera_de_pantalla() and not golpeo:
-                balas_activas.append(bala)
-        
+                balas_activas.append(bala)      
         self.balas = balas_activas
 
     def recibir_daño(self, cantidad):
@@ -95,21 +84,31 @@ class Jugador:
         return self.vida <= 0
 
     def dibujar(self, pantalla):
-        if self.sprite:
-            sprite_escalado = pygame.transform.scale(self.sprite, (self.radio * 2, self.radio * 2))
-        # Rotar segun la direccion
-            if self.direccion == "arriba":
-                sprite_final = pygame.transform.rotate(sprite_escalado, 90)
-            elif self.direccion == "abajo":
-                sprite_final = pygame.transform.rotate(sprite_escalado, -90)
-            elif self.direccion == "izquierda":
-                sprite_final = pygame.transform.flip(sprite_escalado, True, False)
-            else:  # derecha
-                sprite_final = sprite_escalado
+        pygame.draw.rect(pantalla, AZUL, self.rect)
+        pygame.draw.rect(pantalla, ROJO, (self.x - 20, self.y - 30, 40, 5))
+        pygame.draw.rect(pantalla, VERDE, (self.x - 20, self.y - 30, 40 * (self.vida / self.vida_maxima), 5))
 
-            pantalla.blit(sprite_final, (self.x - self.radio, self.y - self.radio))
-        else:
-            pygame.draw.circle(pantalla, AZUL, (int(self.x), int(self.y)), self.radio)
+        if self.robo_vida_activo:
+            pygame.draw.circle(pantalla, (255, 0, 255, 50), (int(self.x), int(self.y)), self.radio_robo_vida, 2)
+    
+    def activar_robo_vida(self):
+        if not self.robo_vida_activo:
+            self.robo_vida_activo = True
+            self.tiempo_robo_vida = self.duracion_robo
+            self.color = MORADO
+            return True
+        return False
+    
+    def robar_vida(self, enemigos):
+        if not self.robo_vida_activo:
+            return           
+        self.tiempo_robo_vida -= 1
+        if self.tiempo_robo_vida <= 0:
+            self.robo_vida_activo = False
+            self.color = AZUL
+            return
         
-            pygame.draw.rect(pantalla, ROJO, (self.x - 20, self.y - 30, 40, 5))
-            pygame.draw.rect(pantalla, VERDE, (self.x - 20, self.y - 30, 40 * (self.vida/self.vida_maxima), 5))
+        for enemigo in enemigos[:]: 
+            if enemigo.esta_muerto() and self.rect.colliderect(enemigo.rect):
+                self.vida = min(self.vida + 10, self.vida_maxima)
+                enemigos.remove(enemigo)
