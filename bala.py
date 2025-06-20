b382b5eb60ff9a91c00aa557ae79e3bb07a329ac
Choco-c4t <138539780+Choco-c4t@users.noreply.github.com>
2025-06-19 23:16:05 -0500

    bala.py

--- bala.py
+++ bala.py
@@ -7,21 +7,24 @@ class Bala:
         self.y = y
         self.dx = dx * 7 
         self.dy = dy * 7  
-        self.radio = 5
         self.enemigo = enemigo  # True si es una bala enemiga
         self.color = ROJO if enemigo else AZUL  # Rojo para enemigos, azul para jugador
+        self.width = 10
+        self.height = 10
+        self.rect = pygame.Rect(self.x - self.width//2, self.y - self.height//2, self.width, self.height)
+
 
     def actualizar(self):
         self.x += self.dx
         self.y += self.dy
+        self.rect.center = (self.x, self.y)
 
     def fuera_de_pantalla(self):
-        return (self.x < -self.radio or self.x > ANCHO + self.radio or
-                self.y < -self.radio or self.y > ALTO + self.radio)
+        return (self.x < -self.width or self.x > ANCHO + self.width or
+                self.y < -self.height or self.y > ALTO + self.height)
 
     def dibujar(self, pantalla):
-        pygame.draw.circle(pantalla, self.color, (int(self.x), int(self.y)), self.radio)
+        pygame.draw.rect(pantalla, self.color, self.rect)
 
     def verificar_colision(self, objeto):
-        distancia = ((self.x - objeto.x)**2 + (self.y - objeto.y)**2)**0.5
-        return distancia < self.radio + objeto.radio
+        return self.rect.colliderect(objeto.rect)
