efbe8c339b5d76b191be2ea2f23e432c3cf77b32
Choco-c4t <138539780+Choco-c4t@users.noreply.github.com>
2025-06-22 15:24:20 -0500

    config.py

--- config.py
+++ config.py
@@ -1,27 +1,27 @@
 import pygame
-
 ANCHO = 800
 ALTO = 600
 FPS = 60
 
-
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 ROJO = (255, 0, 0)
+ROJO_OSCURO = (200, 0, 0)
 VERDE = (0, 255, 0)
 AZUL = (0, 0, 255)
 AMARILLO = (255, 255, 0)
 MARRON = (128, 0, 0)
-GRIS = (192, 192, 192)
+GRIS = (169,169,169)
 NARANJA = (255, 165, 0)
 MORADO = (160, 32, 240)
 ROSADO = (248, 131, 121)
+DORADO = (227,168,87)
 
-
-SPRITE_JUGADOR = None #pygame.image.load("assets/jugador.png").convert_alpha()
-SPRITE_ENEMIGO_BASE = None #pygame.image.load("assets/enemigo_base.png").convert_alpha()
-SPRITE_ENEMIGO_DISPARA = None #pygame.image.load("assets/enemigo_dispara.png").convert_alpha()
-SPRITE_ENEMIGO_INVOCADOR = None #pygame.image.load("assets/enemigo_invocador.png").convert_alpha()
-SPRITE_MINION = None #pygame.image.load("assets/minion.png").convert_alpha()
-SPRITE_JEFE = None #pygame.image.load("assets/jefe.png").convert_alpha()
+imagen_menu = pygame.image.load('assets\imagen_menu.png')
+imagen_ganar = pygame.image.load('assets\imagen_ganar.png')
+imagen_perder = pygame.image.load('assets\imagen_perder.png')
+cancion_menu = 'assets\sonido_menu.wav'
+cancion_nivel = 'assets\sonido_niveles.wav'
+cancion_perder = 'assets\sonido_perder.wav'
+cancion_victoria = 'assets\sonido_victoria.wav'
 
