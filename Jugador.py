c9d713b0f7299cd9891f8db533eab393462849c1
Choco-c4t <138539780+Choco-c4t@users.noreply.github.com>
2025-06-21 22:00:42 -0500

    Jugador.py

--- Jugador.py
+++ Jugador.py
@@ -2,6 +2,7 @@ import pygame
 from config import *
 from bala import Bala
 
+
 class Jugador:
     def __init__(self, x, y):
         self.x = x
@@ -19,8 +20,16 @@ class Jugador:
         self.tiempo_robo_vida = 0  # Rango
         self.duracion_robo = 300
         self.radio_robo_vida = 20
+        self.color_normal = AZUL  # Color normal del jugador
+        self.color_poder = ROSADO  # Color cuando tiene el poder
+        self.color_actual = self.color_normal  # Color actual
+        self.robo_vida_activo = False
+        self.tiempo_poder = 0
+        self.duracion_poder = 300  # 5 segundos a 60 FPS
 
     def mover(self, teclas):
+
+
         if teclas[pygame.K_w]:
             self.y -= self.velocidad
             self.direccion = "arriba"
@@ -36,7 +45,6 @@ class Jugador:
 
         self.rect.center = (self.x, self.y)
         
-        
         if self.x < self.width // 2:
             self.x = self.width // 2
         if self.x > ANCHO - self.width // 2:
@@ -46,22 +54,26 @@ class Jugador:
         if self.y > ALTO - self.height // 2:
             self.y = ALTO - self.height // 2
 
+
+
     def verificar_colision_enemigo(self, enemigo):
         return self.rect.colliderect(enemigo.rect)
 
     def disparar(self):
-        if len(self.balas) <= 5:  # Max 5 balas
+        if len(self.balas) < 3:
+            # Curación al disparar si el poder está activo
+            if self.robo_vida_activo:
+                self.vida = min(self.vida + 5, self.vida_maxima)
+            
+            # Lógica normal de disparo
             if self.direccion == "arriba":
                 self.balas.append(Bala(self.x, self.y, 0, -1))
             elif self.direccion == "abajo":
                 self.balas.append(Bala(self.x, self.y, 0, 1))
             elif self.direccion == "izquierda":
                 self.balas.append(Bala(self.x, self.y, -1, 0))
-            else:  # derecha
+            else:
                 self.balas.append(Bala(self.x, self.y, 1, 0))
-        
-        if self.robo_vida_activo:
-            self.vida = min(self.vida + 5, self.vida_maxima) 
 
     def actualizar_balas(self, enemigos):
         balas_activas = []
@@ -84,31 +96,21 @@ class Jugador:
         return self.vida <= 0
 
     def dibujar(self, pantalla):
-        pygame.draw.rect(pantalla, AZUL, self.rect)
+        color = self.color_poder if self.robo_vida_activo else self.color_normal
+        pygame.draw.rect(pantalla, color, self.rect)
+        
+        # Barra de vida
         pygame.draw.rect(pantalla, ROJO, (self.x - 20, self.y - 30, 40, 5))
         pygame.draw.rect(pantalla, VERDE, (self.x - 20, self.y - 30, 40 * (self.vida / self.vida_maxima), 5))
-
-        if self.robo_vida_activo:
-            pygame.draw.circle(pantalla, (255, 0, 255, 50), (int(self.x), int(self.y)), self.radio_robo_vida, 2)
     
-    def activar_robo_vida(self):
-        if not self.robo_vida_activo:
-            self.robo_vida_activo = True
-            self.tiempo_robo_vida = self.duracion_robo
-            self.color = MORADO
-            return True
-        return False
-    
-    def robar_vida(self, enemigos):
-        if not self.robo_vida_activo:
-            return           
-        self.tiempo_robo_vida -= 1
-        if self.tiempo_robo_vida <= 0:
-            self.robo_vida_activo = False
-            self.color = AZUL
-            return
+    def activar_poder(self):
+        self.robo_vida_activo = True
+        self.tiempo_poder = self.duracion_poder
+        self.color_actual = self.color_poder
         
-        for enemigo in enemigos[:]: 
-            if enemigo.esta_muerto() and self.rect.colliderect(enemigo.rect):
-                self.vida = min(self.vida + 10, self.vida_maxima)
-                enemigos.remove(enemigo)
+    def actualizar_poder(self):
+        if self.robo_vida_activo:
+            self.tiempo_poder -= 1
+            if self.tiempo_poder <= 0:
+                self.robo_vida_activo = False
+                self.color_actual = self.color_normal
